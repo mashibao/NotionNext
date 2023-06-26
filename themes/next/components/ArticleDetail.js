9c7a3e2a07f845091b81735ef14b8a9758651ba6
mashibao <1164882679@qq.com>
2023-06-26 14:13:36 +0800

    Update ArticleDetail.js

--- themes/next/components/ArticleDetail.js
+++ themes/next/components/ArticleDetail.js
@@ -89,7 +89,7 @@ export default function ArticleDetail(props) {
                         data-adtest="on"
                         data-ad-layout="in-article"
                         data-ad-format="fluid"
-                        data-ad-client="ca-pub-2708419466378217"
+                        data-ad-client="ca-pub-5548826292365832"
                         data-ad-slot="3806269138" />
                 </section>
 
